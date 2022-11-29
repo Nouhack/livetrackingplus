0fe59dd266874a624816f9d794cbc80f09947698
Nouhack <nouh.saiche@gmail.com>
2022-11-29 14:42:05 +0100

    init13

--- components/Pricing.js
+++ components/Pricing.js
@@ -1,9 +1,7 @@
 import React, { useMemo } from "react";
 import { ReactComponent as ReactLogo } from "../public/assets/feature.svg";
-import Image from "next/image";
 import Testimoni from "./Testimoni";
 import ButtonPrimary from "./misc/ButtonPrimary";
-import ButtonOutline from "./misc/ButtonOutline.";
 import Maps from "../public/assets/HugeGlobal.svg";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
